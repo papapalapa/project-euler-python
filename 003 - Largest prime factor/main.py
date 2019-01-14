e38754e38654e12bce8c8697c9b1615ada5b57dd
Janghoon Lee <janghoon@minerva.kgi.edu>
2019-01-14 15:01:10 -0800

    Construct a class for optimized recursion

--- 003 - Largest prime factor/main.py
+++ 003 - Largest prime factor/main.py
@@ -4,6 +4,11 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 class Problem3:
+    def __init__(self, dividend):
+        self.primeList = self.primeNumbersList(10000)
+        self.primeFactors = []
+        self.dividend = dividend
+
     # Sieve of Eratosthenes
     def primeNumbersList(self, target):
         numbersList = [i for i in range(2, target + 1)]
@@ -15,5 +20,15 @@ class Problem3:
                 if j % i == 0 and j != i:
                     del numbersList[numbersList.index(j)]
         return numbersList
-            
-print(Problem3().primeNumbersList(10000))
+
+    def largestPrimeFactor(self):
+        for primeNumber in self.primeList:
+            if self.dividend % primeNumber == 0:
+                self.primeFactors.append(primeNumber)
+                self.dividend /= primeNumber
+                self.largestPrimeFactor()
+        
+        return self.primeFactors, self.dividend, max(self.primeFactors)
+
+
+print(Problem3(600851475143).largestPrimeFactor())
