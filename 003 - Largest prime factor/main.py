9a96cffb7f2c4d26026d46bc28f749d238f84ccc
Janghoon Lee <janghoon@minerva.kgi.edu>
2019-01-13 22:44:37 -0800

    Optimze sieve of eratosthenes function implementation

--- 003 - Largest prime factor/main.py
+++ 003 - Largest prime factor/main.py
@@ -5,14 +5,15 @@ What is the largest prime factor of the number 600851475143 ?
 """
 class Problem3:
     # Sieve of Eratosthenes
-    def primeNumbersList(self, limit):
-        numbersList = [i for i in range(2, limit + 1)]
-        for i in range(2, int(limit**0.5) + 1):
-            prime = i
+    def primeNumbersList(self, target):
+        numbersList = [i for i in range(2, target + 1)]
+        terminatorNumber = int((len(numbersList) + 1)**0.5) + 1
+        for i in numbersList:
+            if (i**2 > target):
+                break
             for j in numbersList:
-                if j % prime == 0 and j != prime:
+                if j % i == 0 and j != i:
                     del numbersList[numbersList.index(j)]
-        
         return numbersList
             
-print(Problem3().primeNumbersList(100))
+print(Problem3().primeNumbersList(10000))
