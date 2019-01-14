dca11cff627fe51690817ce582b82ee3dbe2f194
Janghoon Lee <janghoon@minerva.kgi.edu>
2019-01-13 19:54:36 -0800

    Add prime number listing function

--- 003 - Largest prime factor/main.py
+++ 003 - Largest prime factor/main.py
@@ -9,6 +9,10 @@ class Problem3:
         numbersList = [i for i in range(2, limit + 1)]
         for i in range(2, int(limit**0.5) + 1):
             prime = i
-        return prime
+            for j in numbersList:
+                if j % prime == 0 and j != prime:
+                    del numbersList[numbersList.index(j)]
+        
+        return numbersList
             
-print(Problem3().primeNumbersList(10))
+print(Problem3().primeNumbersList(100))
