6370565b8367fa04f7da52b1f0b69f190dc160c4
Janghoon Lee <janghoon@minerva.kgi.edu>
2019-01-16 00:22:13 -0800

    Add palindrome test function

---
+++ 004 - Largest palindrome product/main.py
@@ -0,0 +1,36 @@
+"""
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+class Problem4:
+    def factorHandler(self):
+        numbers = [1000, 1000]
+        while numbers[0] is not 0:
+            if (numbers[0] > numbers[1]):
+                numbers[0] = numbers[0] - 1
+            else:
+                numbers[1] = numbers[1] - 1
+            print(self.palindromeTest(numbers[0] * numbers[1]))
+
+    def palindromeTest(self, number):
+        number = str(number)
+        digitNumber = len(number)
+        #results = []
+        for i in range(int(digitNumber / 2)):
+            result = number[i] == number[digitNumber - i - 1]
+            if (result == True):
+                # results.append(result)
+                continue
+            else:
+                return False
+        
+        # if (results == []):
+        #     print(number)
+        # print(results)
+        return True
+
+            
+
+Problem4().factorHandler()
