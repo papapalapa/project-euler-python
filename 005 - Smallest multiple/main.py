654c0307970289f985a255fab37cc4ea04bca38d
Janghoon Lee <janghoon@minerva.kgi.edu>
2019-01-19 22:09:18 -0800

    Add Sieve of Eratosthenes for prime numbers generation

--- 005 - Smallest multiple/main.py
+++ 005 - Smallest multiple/main.py
@@ -3,3 +3,12 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
+class Problem5:
+    # Sieve of Eratosthenes
+    def primeList(self, limit):
+        numberList = [i for i in range(2, limit + 1)]
+        for i in range(2, int(limit**0.5) + 1):
+            for number in numberList:
+                if number % i == 0 and number != i:
+                    del numberList[numberList.index(number)]
+        return numberList
